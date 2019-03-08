72d4f89f57c35df76ab462f2ef8742deda213ff1
Javin-Ambridge <javin.ambridge@gmail.com>
2019-03-07 22:26:16 -0500

    Added Response headers

--- app.js
+++ app.js
@@ -328,6 +328,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    var headers = req.headers['access-control-request-headers']; 
+    if (headers) {
+      res.setHeader('Access-Control-Allow-Headers', headers);
+    }
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 
