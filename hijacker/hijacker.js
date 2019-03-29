af71a815a297f3a43f5cb7d9bd520d5dcb6ac917
Javin-Ambridge <javin.ambridge@gmail.com>
2019-03-28 20:24:36 -0400

    Fixing quality selection bug

--- hijacker/hijacker.js
+++ hijacker/hijacker.js
@@ -101,7 +101,7 @@ module.exports.startHijack = function(userID, gameName, twitchStream, downloadID
 			  		var epoch = (new Date).getTime();
 			  		fileName = (ORIGIN_PATH + "video_data_hijacks/" + gameName + "/") + userID + "-finished-" + epoch;
 
-			  		var downloadCMD = ffmpegPath + ' -i $(' + ORIGIN_PATH + 'youtube-dl -f ("bestvideo[width>=1920]"/bestvideo)+bestaudio/best -g ' + twitchStream + ') -c copy -preset medium ' + fileName + '.mp4';
+			  		var downloadCMD = ffmpegPath + ' -i $(' + ORIGIN_PATH + 'youtube-dl -f \\(\"bestvideo[width>=1920]\"/bestvideo\\)+bestaudio/best -g ' + twitchStream + ') -c copy -preset medium ' + fileName + '.mp4';
 			  		cProcess = shell.exec(downloadCMD, {async: true});
 
 			  		return setTimeout(function() {
