b828bf8c6a1eb7e46f7779f80d14180cc869e6e4
Javin-Ambridge <javin.ambridge@gmail.com>
2019-06-16 15:39:58 -0400

    Loading NVM before starting nodejs install

--- docker_info/setup_encoding_env.sh
+++ docker_info/setup_encoding_env.sh
@@ -6,6 +6,9 @@ echo "" > ~/.bash_profile
 # Install Nodejs
 eval "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash"
 eval ". ~/.nvm/nvm.sh"
+eval "export NVM_DIR=\"$HOME/.nvm\""
+eval "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\""  # This loads nvm
+eval "[ -s \"$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\""  # This loads nvm bash_completion"
 eval "nvm install 8.10"
 
 # Install Git
